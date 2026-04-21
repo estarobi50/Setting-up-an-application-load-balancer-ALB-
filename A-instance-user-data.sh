32da4d68fbfcc59b4bc0828c4d9d9f1bd4e1a0b9
DigitalCulture <DigitalCultureits@gmail.com>
2026-04-21 08:07:26 -0400

    Create A-instance-user-data.sh for HTTPD setup
    This script sets up an HTTPD server, updates packages, and creates index.html files for the default and app pages.

---
+++ A-instance-user-data.sh
@@ -0,0 +1,62 @@
+#!/bin/bash  
+
+# Allowing YUM updates and installations from cloud_user  
+echo '%password%' | passwd cloud_user --stdin  
+yum update -y  
+yum install -y httpd  
+
+# Starting and enabling HTTPD service  
+systemctl start httpd.service  
+systemctl enable httpd.service  
+
+# Adding HTTPD group and adding cloud_user  
+groupadd www  
+usermod -a -G www cloud_user  
+
+# Get the IMDSv2 token  
+TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`  
+
+# Setting homepage index.html file.
+cat <<EOF > /var/www/html/index.html
+<html>
+    <title>Pluralsight Prep</title>
+    <body>
+        <p>Our default homepage</p>
+    </body>
+<html>
+EOF
+
+mkdir -p /var/www/html/a/
+
+# Setting the app index.html file.
+cat <<EOF > /var/www/html/a/index.html
+<html>
+    <style>
+    
+    .center {
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;
+    }
+    </style>
+    <title>Pluralsight Prep</title>
+    <body>
+        <span style="padding-top: 200px;"></span>
+        <img src="https://d31xik8pupf3da.cloudfront.net/public-assets/ps-logo.png" style="max-width: 400px; display: block; margin-left: auto; margin-right: auto; width: 50%;"/>
+        <span style="padding-top: 100px;"></span>
+        <p></p>
+        <p></p>
+    </body>
+<html>
+EOF
+
+# Creating simple index.html file for HTTPD base page  
+echo '<html><h1>Hello Gurus! <strong>I am hosting application A!</strong></h1><h2>I live in this Availability Zone: ' >> /var/www/html/a/index.html 
+curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone >> /var/www/html/a/index.html
+echo '</h2> <h2>I go by this Instance Id: ' >> /var/www/html/a/index.html
+curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id >> /var/www/html/a/index.html  
+echo '</2></html> ' >> /var/www/html/a/index.html
+
+# Restarting HTTPD service to enforce new index.html just to be safe.  
+systemctl restart httpd.service
